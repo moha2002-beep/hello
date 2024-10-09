595415bafd7e03173ae245f67f532d7e826c89af
Mohamed ahmed <amii1nahmed2002@gmail.com>
2024-10-09 12:10:53 +0100

    initial version

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world ");
+var  http = require("http");
+const port = 8000;
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end("<!doctype html><html><head><title>My Server</title></head><body><h1>Hello, World!</h1></body></html>");
+}).listen(port, function() {
+    console.log(Node server is running on port ${port});
+});
